d69f3380c7cc66ece41c2ac3d0a614f29a73b6c9
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-10-31 15:02:41 +0700

    update register

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -74,13 +74,6 @@ export default function Loginpage() {
             Sign in to your account
           </button>
         </form>
-        {/* <Link
-          type="button"
-          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2"
-          href="/"
-        >
-          Back
-        </Link> */}
         <button
           type="button"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2"
--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -1,77 +1,91 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Registerpage() {
+  const router = useRouter();
+
+  const Back = () => {
+    router.back();
+  };
+
   return (
     <div className="relative grid h-screen place-items-center">
-      <div className="align-center w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" action="#">
-          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
+      <div className="align-center w-full max-w-sm rounded-lg border shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <form className="space-y-6">
+          <h5 className="text-xl font-medium dark:text-white text-center">
+            Register
           </h5>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Your email
+            <label className="block mb-2 text-sm font-medium dark:text-gray-300">
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="usename"
+              className="border text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Enter Username"
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium dark:text-gray-300">
+              Email
             </label>
             <input
               type="email"
               name="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="border text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               placeholder="name@company.com"
               required
             />
           </div>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Your password
+            <label className="block mb-2 text-sm font-medium dark:text-gray-300">
+              Password
             </label>
             <input
               type="password"
               name="password"
               id="password"
               placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              minLength={8}
+              className="border text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
             />
           </div>
-          <div className="flex items-start">
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                  required
-                />
-              </div>
-              <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                Remember me
-              </label>
-            </div>
-            <a
-              href="#"
-              className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Lost Password?
-            </a>
+          <div>
+            <label className="block mb-2 text-sm font-medium dark:text-gray-300">
+              Confirm password
+            </label>
+            <input
+              type="password"
+              name="confirmpassword"
+              id="password"
+              placeholder="••••••••"
+              minLength={8}
+              className="border text-sm rounded-lg block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              required
+            />
           </div>
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="w-full text-white hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
           >
-            Login to your account
+            Submit
           </button>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?{" "}
-            <a
-              href="#"
-              className="text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Create account
-            </a>
-          </div>
         </form>
+        <button
+          type="button"
+          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2"
+          onClick={Back}
+        >
+          Back
+        </button>
       </div>
     </div>
   );
