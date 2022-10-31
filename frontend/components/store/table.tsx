b8f99060e24bc0843c3c04e51b09bef5a00f1df8
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-10-31 12:30:27 +0700

    update

--- frontend/components/store/table.tsx
+++ frontend/components/store/table.tsx
@@ -92,13 +92,13 @@ export default function Table() {
             <td className="py-4 px-6">
             <button
                 type="button"
-                className="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                className="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
               >
              edit
               </button>
               <button
                 type="button"
-                className="focus:outline-none text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                className="focus:outline-none text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:ring-red-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
               >
               Delete
               </button>
