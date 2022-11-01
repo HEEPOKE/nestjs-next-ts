a91e1088b862d41eae513c3b98e11499c37a3abc
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-01 08:35:20 +0700

    Update formInput.tsx

--- frontend/components/create/formInput.tsx
+++ frontend/components/create/formInput.tsx
@@ -18,13 +18,11 @@ export default function FormInput() {
         <div className="w-10/12 bg-gray-800 rounded-lg mt-3">
           <form>
             <div className="ml-3 mr-3 mt-2">
-              <label className="block mb-2 font-extrabold text-lg text-white">
-                name
-              </label>
+              <label className="block mb-2 font-extrabold text-lg text-white">name</label>
               <input
                 type="text"
                 name="name"
-                className="text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                className="text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
                 placeholder="Enter Name"
                 required
               />
@@ -36,7 +34,7 @@ export default function FormInput() {
               <input
                 type="text"
                 name="color"
-                className="text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                className="text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
                 placeholder="Enter Color"
                 required
               />
@@ -48,7 +46,7 @@ export default function FormInput() {
               <input
                 type="text"
                 name="category"
-                className="text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                className="text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
                 placeholder="Enter Category"
                 required
               />
@@ -60,7 +58,7 @@ export default function FormInput() {
               <input
                 type="number"
                 name="price"
-                className="text-white text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                className="text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
                 placeholder="Enter Price"
                 min={0}
                 required
@@ -73,12 +71,15 @@ export default function FormInput() {
               <input
                 className="block mb-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                 type="file"
+                name="image"
+                accept="image/*"
+                multiple
               />
             </div>
             <div className="flex justify-center">
               <button
                 type="submit"
-                className="w-1/3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                className="text-white w-1/3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
               >
                 Save
               </button>
@@ -87,7 +88,7 @@ export default function FormInput() {
           <div className="flex justify-center">
             <button
               type="button"
-              className="w-1/3 text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+              className="text-white w-1/3 bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
               onClick={Back}
             >
               Back
