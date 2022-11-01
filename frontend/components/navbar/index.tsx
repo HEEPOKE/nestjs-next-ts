292decbd1bf184c46c8df2c22d76497991fae486
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-01 09:09:43 +0700

    update nacbar

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,46 +1,115 @@
+'use client';
+
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Menu() {
+  const router = useRouter();
+
+  const LoginButton = () => {
+    router.push('/auth/login');
+  };
+
+  const RegisterButton = () => {
+    router.push('/auth/register');
+  };
+
+
   return (
-    <nav className="bg-green-400 overflow-hidden">
-      <Link href="/">
-        <li className="text-center float-left pl-6 pr-6 pt-2 pb-2 font-medium text-2xl text-zinc-50 cursor-pointer block no-underline">
-          TOPIC
-        </li>
-      </Link>
-      <ul className="m-0 p-0 list-none">
-        <Link href="/">
-          <li className="text-center float-left pl-2 pr-2 pt-2 pb-2 font-medium text-2xl text-zinc-50 hover:bg-green-800 cursor-pointer block no-underline">
-            Home
-          </li>
+    <nav className="border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-900">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/img/Logo.jpg"
+            className="mr-3 h-6 sm:h-9 rounded-full"
+            width={50}
+            height={270}
+            alt="Logo"
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Next App
+          </span>
         </Link>
-        <Link href="/create">
-          <li className="text-center float-left pl-2 pr-2 pt-2 pb-2 font-medium text-2xl text-zinc-50 hover:bg-green-800 cursor-pointer block no-underline">
-           Create
-          </li>
-        </Link>
-        <Link href="/store">
-          <li className="text-center float-left pl-2 pr-2 pt-2 pb-2 font-medium text-2xl text-zinc-50 hover:bg-green-800 cursor-pointer block no-underline">
-            Store
-          </li>
-        </Link>
-        <Link href="/about">
-          <li className="text-center float-left pl-2 pr-2 pt-2 pb-2 font-medium text-2xl text-zinc-50 hover:bg-green-800 cursor-pointer block no-underline">
-            About
-          </li>
-        </Link>
-        <Link href="/auth/register">
-          <li className="float-right pl-2 pr-2 pt-2 pb-2 font-medium text-2xl text-center text-zinc-50 cursor-pointer no-underline block hover:bg-green-800">
-            Register
-          </li>
-        </Link>
-        <Link href="/auth/login">
-          <li className="float-right pl-2 pr-2 pt-2 pb-2 font-medium text-2xl text-center text-zinc-50 cursor-pointer no-underline block hover:bg-green-800">
+        <div className="flex md:order-2">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-auto sm:mr-1"
+            onClick={LoginButton}
+          >
             Login
-          </li>
-        </Link>
-      </ul>
+          </button>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 ml-3"
+            onClick={RegisterButton}
+          >
+            Regiater
+          </button>
+          <button
+            data-collapse-toggle="navbar-cta"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-cta"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div
+          className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                href="/"
+                className="block text-2xl py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about"
+                className="block text-2xl py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/store"
+                className="block text-2xl py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+              >
+                Store
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/create"
+                className="block text-2xl py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+              >
+                Create
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
